d64e57d6eefc4ad0967d1064a4b6a340bccbe531
AkhilKrish1224 <akhilkrishnan1224@gmail.com>
2024-09-25 02:23:33 +0530

    Working on GetStaticPaths

--- pages/customers/[id].tsx
+++ pages/customers/[id].tsx
@@ -1,10 +1,51 @@
-import { NextPage } from "next";
+import { GetStaticPaths, GetStaticProps, NextPage } from "next";
 import { useRouter } from "next/router";
+import { Customer } from "./index";
+//Error : Import 'Customer' conflicts with local value, so must be declared with a type-only import when 'isolatedModules' is enabled.
+//Rename last default export to fix this
+import axios from "axios";
+import { ParsedUrlQuery } from "querystring";
 
-const Customer: NextPage = () => {
-  const router = useRouter();
-  const { id } = router.query;
-  return <h1>Customer {id}</h1>;
+type Props = {
+  customer: Customer;
+};
+
+interface Params extends ParsedUrlQuery {
+  id: string;
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const result = await axios.get("http://localhost:8000/api/customers/");
+  const paths = result.data.customers.map((customer: Customer) => {
+    return {
+      params: { id: customer.id.toString() },
+    };
+  });
+
+  return {
+    paths: paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps<Props, Params> = async (
+  context
+) => {
+  const params = context.params!;
+
+  const result = await axios.get<{ customer: Customer }>(
+    `http://localhost:8000/api/customers/${params.id}`
+  );
+  console.log(result);
+  return {
+    props: {
+      customer: result.data.customer,
+    },
+  };
+};
+
+const Customer: NextPage<Props> = (props) => {
+  return <h1>Customer {props.customer.name}</h1>;
 };
 
 export default Customer;
