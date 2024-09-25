0041832c28a6742ef56b04e80fee46ff463bcffc
AkhilKrish1224 <akhilkrishnan1224@gmail.com>
2024-09-25 18:39:53 +0530

    Add and Connect to MongoDB

--- pages/customers/index.tsx
+++ pages/customers/index.tsx
@@ -1,21 +1,30 @@
 import { NextPage, GetStaticProps, InferGetStaticPropsType } from "next";
 import axios from "axios";
+import { MongoClient, ObjectId } from "mongodb";
 
 export type Customer = {
-  id: number;
+  _id: ObjectId;
   name: string;
   industry: string;
 };
 
 export const getStaticProps: GetStaticProps = async () => {
-  const result = await axios.get<{
-    customers: Customer[];
-  }>("http://localhost:8000/api/customers/");
-  console.log(result.data.customers);
+  const mongoClient = new MongoClient(
+    "mongodb+srv://admin:hw2sgvKahyDs3xZA@customers.lzuf3.mongodb.net/Customers?retryWrites=true&w=majority&appName=Customers"
+  );
+
+  const data = await mongoClient.db().collection("Customers").find().toArray();
+
+  console.log("!!!", data);
+
+  //   const result = await axios.get<{
+  //     customers: Customer[];
+  //   }>("http://localhost:8000/api/customers/");
+  //   console.log(result.data.customers);
 
   return {
     props: {
-      customers: result.data.customers,
+      customers: JSON.parse(JSON.stringify(data)),
     },
     revalidate: 60,
   };
@@ -29,10 +38,10 @@ const Customers: NextPage = ({
       <h1>Customers</h1>
       {customers.map((customer: Customer) => {
         return (
-          <div key={customer.id}>
+          <div key={customer._id.toString()}>
             <p>{customer.name}</p>
             <p>{customer.industry}</p>
-            <p>{customer.id}</p>
+            <p>{customer._id.toString()}</p>
           </div>
         );
       })}
